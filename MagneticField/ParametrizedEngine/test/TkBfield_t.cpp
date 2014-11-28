7896879e6c2a73c04b014744f49b4d335540f599
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-11-28 10:57:45 +0100

    make vector lenght a parameter

--- MagneticField/ParametrizedEngine/test/TkBfield_t.cpp
+++ MagneticField/ParametrizedEngine/test/TkBfield_t.cpp
@@ -4,6 +4,10 @@
 
 #include<iostream>
 
+// performance test
+#include "DataFormats/Math/test/rdtscp.h"
+
+
 int main() {
 
   TkBfield f;
@@ -14,6 +18,17 @@ int main() {
      f.getBxyz(x,b);
      std::cout << r <<',' << z << " : " << b[1] << ',' << b[2] << std::endl;  
    } 
+
+
+  float btot[3] = {};
+  unsigned long long t = -rdtsc();
+  for (float r=0; r<1.20f; r+=0.001f)
+   for (float z=-3.0f; z<3.0f; z+=0.002f) {
+     float x[3] = {0.f,r,z}; float b[3];
+     f.getBxyz(x,b); btot[1]+=b[1]; btot[2]+=b[2]; 
+   }
+  t += rdtsc();
+  std::cout << "time " << t << ' ' << btot[1] <<','<<btot[2]<< std::endl;  
 
   return 0;
 
