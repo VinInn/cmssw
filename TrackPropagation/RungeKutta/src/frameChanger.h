09c7b9c1258cd56a9827c7729e39853a7605e2fd
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-11-30 19:44:22 +0100

    negligible regression

---
+++ TrackPropagation/RungeKutta/src/frameChanger.h
@@ -0,0 +1,48 @@
+#ifndef RKframeChanger_H
+#define RKframeChanger_H
+
+#include "FWCore/Utilities/interface/Visibility.h"
+#include "DataFormats/GeometrySurface/interface/Plane.h"
+
+namespace frameChanger dso_internal {
+
+  /** Moves the first argument ("plane") to the reference frame given by the second
+   *  argument ("frame"). The returned frame is not positioned globally!
+   */
+    template <typename T, typename U>
+    HessianPlane<T> transformHessianPlane( const HessianPlane<T>& plane, const GloballyPositioned<U>& frame) {
+      using Frame =  GloballyPositioned<U>;
+      return HessianPlane<T>( frame.toLocal(Frame::GlobalVector(plane.basicVector())).basicVector(), plane.localZ(frame.position().basicVector()) );
+    }
+
+  /** Moves the first argument ("plane") to the reference frame given by the second 
+   *  argument ("frame"). The returned frame is not positioned globally!
+   */
+    template <typename T>
+    Plane transformPlane( const Plane& plane, const GloballyPositioned<T>& frame) {
+        typedef GloballyPositioned<T>                  Frame;
+	typename Plane::RotationType rot = plane.rotation() * frame.rotation().transposed();
+	typename Frame::LocalPoint lpos = frame.toLocal(plane.position());
+	typename Plane::PositionType pos( lpos.basicVector()); // cheat!
+	return Plane(pos, rot);
+    }
+
+
+/** Moves the first argument ("plane") to the reference frame given by the second 
+ *  argument ("frame"). The returned frame is not positioned globally!
+ */
+    template <typename T, typename U>
+    GloballyPositioned<T> toFrame( const GloballyPositioned<T>& plane, 
+				   const GloballyPositioned<U>& frame) {
+	typedef GloballyPositioned<T>                  Plane;
+	typedef GloballyPositioned<U>                  Frame;
+
+	typename Plane::RotationType rot = plane.rotation() * frame.rotation().transposed();
+	typename Frame::LocalPoint lpos = frame.toLocal(plane.position());
+	typename Plane::PositionType pos( lpos.basicVector()); // cheat!
+	return Plane( pos, rot);
+
+    }
+}
+
+#endif
