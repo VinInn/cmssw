4b3cb803f5f96bfb91ef9984ed9c042e6cc8e456
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-12-09 11:29:49 +0100

    test long distance, and time

--- TrackingTools/GeomPropagators/test/HelixPropagators_t.cpp
+++ TrackingTools/GeomPropagators/test/HelixPropagators_t.cpp
@@ -5,7 +5,7 @@
 #include "TrackingTools/GeomPropagators/interface/HelixArbitraryPlaneCrossing2Order.h"
 #include "TrackingTools/GeomPropagators/interface/HelixArbitraryPlaneCrossing.h"
 #include "TrackingTools/GeomPropagators/interface/StraightLinePlaneCrossing.h"
-
+#include "DataFormats/Math/test/rdtscp.h"
 
 
 #include <algorithm>
@@ -68,7 +68,7 @@ void crossing1() {
 
   GlobalPoint  pos(-2.95456,-48.2127,3.1033);
   
-  double rho = 0.00223254;
+  float rho = 0.00223254;
   
   Surface::RotationType rot(0.995292,0.0969201,0.000255868,
 			    8.57131e-06,0.00255196,-0.999997,
@@ -129,7 +129,7 @@ void crossing2() {
   
   Plane plane(pos,rot);
   
-  double rho = 0.00223254;
+  float rho = 0.00223254;
   
   bool cross; double s;
 
@@ -169,13 +169,114 @@ void crossing2() {
 
 }
 
+
+
+void crossing3() {
+
+  std::cout << std::endl;
+
+  GlobalPoint startingPos(-8.12604,-50.829,9.82116);
+  GlobalVector startingDir(-0.517536,-5.09581,1.02212);
+
+    startingPos-= 10*startingDir;
+
+
+  GlobalPoint  pos(-2.96723,-51.4573,14.8322);
+
+  Surface::RotationType rot(0.995041,0.0994701,0.000124443,
+                            0.000108324,-0.00233467,0.999997,
+                            0.0994701,-0.995038,-0.00233387);
+  std::cout << rot << std::endl;
+
+  Plane plane(pos,rot);
+
+  float rho = 0.00223254;
+
+  bool cross; double s;
+
+
+  StraightLinePlaneCrossing  line(startingPos.basicVector(), startingDir.basicVector(), alongMomentum);
+  std::tie(cross,s) = line.pathLength(plane);
+  std::cout << s << ' ' << plane.toLocal(GlobalPoint(line.position(s))) << " " << std::endl;
+ 
+
+  HelixArbitraryPlaneCrossing arbitrary(startingPos.basicVector(), startingDir.basicVector(), rho,alongMomentum);
+  HelixArbitraryPlaneCrossing2Order arbitrary2O(startingPos.basicVector(), startingDir.basicVector(), rho,alongMomentum);
+
+  std::tie(cross,s) = arbitrary.pathLength(plane);
+  std::cout << s << ' ' << plane.toLocal(GlobalPoint(arbitrary.position(s))) << " " <<  plane.toLocal(GlobalVector(arbitrary.direction(s))) << std::endl;
+
+  std::tie(cross,s) = arbitrary2O.pathLength(plane);
+  std::cout << s << ' ' << plane.toLocal(GlobalPoint(arbitrary2O.position(s))) << " " <<  plane.toLocal(GlobalVector(arbitrary2O.direction(s))) << std::endl;
+
+ 
+
+
+  HelixBarrelPlaneCrossingByCircle precise(startingPos, startingDir, rho,alongMomentum);
+  std::tie(cross,s) = precise.pathLength(plane);
+  
+  HelixBarrelPlaneCrossing2OrderLocal crossing(startingPos, startingDir, rho, plane);
+
+  std::cout << s<< ' ' << plane.toLocal(GlobalPoint(precise.position(s))) << " " <<  plane.toLocal(GlobalVector(precise.direction(s))) << std::endl;
+  std::cout << HelixBarrelPlaneCrossing2OrderLocal::positionOnly(startingPos, startingDir, rho, plane) << ' ';
+  std::cout << crossing.position() << ' ' << crossing.direction() << std::endl;
+                            
+  LocalPoint thePos; LocalVector theDir;
+  std::tie(thePos,theDir) = secondOrderAccurate(startingPos, startingDir, rho, plane);
+  
+  std::cout << thePos << ' ' << theDir << std::endl;
+  
+  
+
+}
+
+
+
+void timing() {
+
+  GlobalPoint startingPos(-8.12604,-50.829,9.82116);
+  GlobalVector startingDir(-0.517536,-5.09581,1.02212);
+
+  // startingPos-= 10*startingDir;
+
+
+  GlobalPoint  pos(-2.96723,-51.4573,14.8322);
+  
+  Surface::RotationType rot(0.995041,0.0994701,0.000124443,
+                            0.000108324,-0.00233467,0.999997,
+                            0.0994701,-0.995038,-0.00233387);
+  std::cout << rot << std::endl;
+
+  Plane plane(pos,rot);
+  
+  double rho = 0.00223254;
+
+  HelixArbitraryPlaneCrossing arbitrary(startingPos.basicVector(), startingDir.basicVector(), rho,alongMomentum);
+
+  bool cross; double s;
+ 
+  double stot=0;
+  unsigned long long t = -rdtsc();
+  for (int i=0; i<1000; ++i) {
+    std::tie(cross,s) = arbitrary.pathLength(plane);
+    stot+=s;
+  }
+  t+=rdtsc();
+
+
+  std::cout << "time " << double(t)/1000 << ' ' << stot << std::endl;
+
+}
+
+
+
 void testHelixBarrelPlaneCrossing2OrderLocal() {
 
   crossing1();
-
   crossing2();
+  crossing3();
 
-
+  timing();
 
 }
 
