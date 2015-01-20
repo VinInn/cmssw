7c2a466f8ffb869159dc48936f28a988a517ee88
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-01-20 14:52:52 +0100

    parsing c++ flags works

--- VITest/ExprEval/src/ExprEval.cc
+++ VITest/ExprEval/src/ExprEval.cc
@@ -5,6 +5,7 @@
 #include "popenCPP.h"
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <dlfcn.h>
 
 
@@ -18,14 +19,15 @@ namespace {
     return n1;
   }
 
-
+  /*
   std::string cxxflags() {
     std::string ret = 
       "-DGNU_GCC -D_GNU_SOURCE -DPROJECT_NAME='\"CMSSW\"' -DBOOST_SPIRIT_THREADSAFE -DPHOENIX_THREADSAFE -O2 -pthread -pipe -Werror=main -Werror=pointer-arith -Werror=overlength-strings -Wno-vla -Werror=overflow -Wstrict-overflow -std=c++11 -msse3 -ftree-vectorize -Wno-strict-overflow -Werror=array-bounds -Werror=format-contains-nul -Werror=type-limits -fvisibility-inlines-hidden -fno-math-errno --param vect-max-version-for-alias-checks=50 -fipa-pta -Wa,--compress-debug-sections -felide-constructors -fmessage-length=0 -ftemplate-depth-300 -Wall -Wno-non-template-friend -Wno-long-long -Wreturn-type -Wunused -Wparentheses -Wno-deprecated -Werror=return-type -Werror=missing-braces -Werror=unused-value -Werror=address -Werror=format -Werror=sign-compare -Werror=write-strings -Werror=delete-non-virtual-dtor -Werror=maybe-uninitialized -Werror=strict-aliasing -Werror=narrowing -Werror=uninitialized -Werror=unused-but-set-variable -Werror=reorder -Werror=unused-variable -Werror=conversion-null -Werror=return-local-addr -Werror=switch -fdiagnostics-show-option -Wno-unused-local-typedefs -Wno-attributes -Wno-psabi -DBOOST_DISABLE_ASSERTS -fPIC ";
     ret += "-DCMSSW_GIT_HASH='"+ edm::getReleaseVersion() + "' -DPROJECT_VERSION='" + edm::getReleaseVersion() + "' ";
     return ret;
   }
-
+  */
+  
 }
 
 ExprEval::ExprEval(const char * pkg, const char * iname, const char * iexpr) :
@@ -60,8 +62,18 @@ ExprEval::ExprEval(const char * pkg, const char * iname, const char * iexpr) :
   auto arch = edm::getEnvironmentVariable("SCRAM_ARCH");
   auto baseDir = edm::getEnvironmentVariable("CMSSW_BASE");
 
-  std::string cpp = "c++ -H -Wall -shared -Winvalid-pch "; cpp+=cxxflags();
-  cpp += "-I" + baseDir + "/include/" + arch; 
+  std::string cxxf;
+  try{
+    auto ss = popenCPP(std::string("sed 's/-I[^ ]*//g' ") + baseDir + "/include/" + arch + "/" + pkg +"/src/precompile.h.cxxflags");
+    std::getline(*ss,cxxf);
+
+    { std::regex rq("=\""); cxxf = std::regex_replace(cxxf,rq,std::string("='\"")); }
+    { std::regex rq("\" "); cxxf = std::regex_replace(cxxf,rq,std::string("\"' ")); }
+    std::cout << '|' << cxxf << "|\n" << std::endl;
+  }catch(...) { std::cout << "error in popen" << " sed " << cxxf << std::endl;}
+
+  std::string cpp = "c++ -H -Wall -shared -Winvalid-pch "; cpp+=cxxf;
+  cpp += " -I" + baseDir + "/include/" + arch; 
   cpp +=  " -o " + ofile + ' ' + sfile+" 2>&1\n";
 
   std::cout << cpp << std::endl;
@@ -71,7 +83,7 @@ ExprEval::ExprEval(const char * pkg, const char * iname, const char * iexpr) :
     char c;
     while (ss->get(c)) std::cout << c;
     std::cout << std::endl;
-  }catch(...) { std::cout << "error in popen" << cpp << std::endl;}
+  }catch(...) { std::cout << "error in popen " << cpp << std::endl;}
 
   void * dl = dlopen(ofile.c_str(),RTLD_LAZY);
   if (!dl) {
