f99669a15d3af9e163a3132a2e10d851a641efbe
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-02-10 16:05:04 +0100

    we have our list of tracks and hit to remove

--- RecoTracker/FinalTrackSelectors/plugins/SharingHitTrackSelector.cc
+++ RecoTracker/FinalTrackSelectors/plugins/SharingHitTrackSelector.cc
@@ -17,6 +17,8 @@
 #include<cassert>
 #include "DataFormats/Common/interface/fakearray.h"
 
+volatile void * apointer;
+
 namespace {
   class SharingHitTrackSelector final : public edm::global::EDProducer<> {
    public:
@@ -45,43 +47,57 @@ namespace {
   void
   SharingHitTrackSelector::produce(edm::StreamID, edm::Event& evt, const edm::EventSetup&) const {
     auto fakeproduct = std::make_unique<FakeProduct>();
-    Product * product = (Product*)fakeproduct.get();
+    Product & product = *(Product*)fakeproduct.get();
     
     auto share = 
       [](TrackingRecHit const * it, TrackingRecHit const * jt)->bool { return it->sharesInput(jt,TrackingRecHit::some); };
 
-
+    Product pairs;
+    
     edm::Handle<TkView> trackCollectionHandle;
     evt.getByToken(trackTag,trackCollectionHandle);
     auto const & tracks = *trackCollectionHandle;
     int nt = tracks.size();
-    for (int i=0; i<nt-1; ++i) {
+    Product::value_type defaultV = {{-1,0,0,0}};
+    product.resize(tracks.size(),defaultV);
+    for (int i=1; i<nt; ++i) {
       auto const & tk1 = tracks[i];
       auto hb = tk1.recHitsBegin();
       TrackingRecHit const * h1[2] = { (*hb), (*(hb+1)) };
-      for (int j=i+1; j<nt; ++j) {
+      for (int j=0; j<i; ++j) {
         auto const & tk2 = tracks[j];
         auto hb = tk2.recHitsBegin();
         TrackingRecHit const * h2[2] = { (*hb), (*(hb+1)) };
         for (int k1=0;k1<2;++k1) for (int k2=0;k2<2;++k2) {
           if ( share(h1[k1],h2[k2]) ) {
             Product::value_type v = {{i,j,k1,k2}};
-            (*product).push_back(v);
+            pairs.push_back(v);
+	    product[i][0]=i;
+	    product[j][0]=j;
+ 	    product[i][k1+1]=1;
+	    product[j][k2+1]=1;
             break;
           }
         }
       } 
     }
 
-    std::cout << "size " << fakeproduct->size() << '/' << tracks.size() << std::endl;
-    for (auto const & v : *product)
+    product.erase(std::remove_if(product.begin(),product.end(),[](Product::value_type const & v){return -1==v[0];}),product.end());
+
+    std::cout << "size " << product.size() << '/' << tracks.size() << std::endl;
+    for (auto const & v : pairs)
       std::cout << v[0] << ','<<v[1]<<": " << v[2] << ','<<v[3]<<": " 
                << tracks[v[0]].numberOfValidHits() << ',' << tracks[v[1]].numberOfValidHits() <<": "
                << tracks[v[0]].charge() << ',' << tracks[v[1]].charge() <<": "
                << tracks[v[0]].pt() << ',' << tracks[v[1]].pt() <<": "
                  << (*(tracks[v[0]].recHitsBegin()+v[2]))->globalPosition().perp() << std::endl;
+    for (auto const & v : product)
+      std::cout << v[0] << ": " <<v[1]<<"," << v[2] << ','<<v[3]<<std::endl;
 
 
+    // have to make sure "product" is used
+    apointer = &product[0];
+    
     evt.put(std::move(fakeproduct));
 
   }
