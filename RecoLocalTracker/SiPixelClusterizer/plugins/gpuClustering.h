dce8df3819ec208f135db630d3f33a07a36e3e6f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-05-18 19:16:58 +0200

    avoid resetting min

--- RecoLocalTracker/SiPixelClusterizer/plugins/gpuClustering.h
+++ RecoLocalTracker/SiPixelClusterizer/plugins/gpuClustering.h
@@ -78,10 +78,10 @@ namespace gpuClustering {
     if (first>=msize) return;
 
     int jmax[10];
-    int jmin[10];
+    // int jmin[10];
     auto niter = (msize-first)/blockDim.x;
     assert(niter<10);
-    for (int k=0; k<niter+1; ++k) jmin[k]=first+k*blockDim.x+1;    
+    // for (int k=0; k<niter+1; ++k) jmin[k]=first+k*blockDim.x+1;    
     for (int k=0; k<niter+1; ++k) jmax[k]=msize;
 
     while (go) {
@@ -95,15 +95,16 @@ namespace gpuClustering {
 	if (id[i]==InvId) continue;  // not valid
 	assert(id[i]==me); //  break;  // end of module
 	++debug[i];
-        auto js = jmin[k]; // i+1; // jmin[k];
+        auto js = i+1;
+//        auto js = jmin[k]; 
         auto jm = jmax[k];
         jmax[k]=i+1;
-        bool first = true;
+//        bool first = true;
 	for (int j=js; j<jm; ++j) {
  	  if (id[j]==InvId) continue;  // not valid
           if (std::abs(int(x[j])-int(x[i]))>1 |
 	      std::abs(int(y[j])-int(y[i]))>1) continue;
-          if (first) {jmin[k] = j; first=false;}
+//          if (first) {jmin[k] = j; first=false;}
 	  auto old = atomicMin(&clus[j],clus[i]);
 	  if (old!=clus[i]) go=true;
 	  atomicMin(&clus[i],old);
