5af3de3321e5de98a1a43913b3bed2e928eb1c74
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-12-08 11:44:07 +0100

    simplify(?) comment

--- RecoLocalTracker/SiPixelRecHits/test/RecHitSoATest.cc
+++ RecoLocalTracker/SiPixelRecHits/test/RecHitSoATest.cc
@@ -18,11 +18,19 @@
 #include "FWCore/Utilities/interface/RunningAverage.h"
 #include "HeterogeneousCore/CUDACore/interface/CUDAScopedContext.h"
 #include "HeterogeneousCore/CUDAServices/interface/CUDAService.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/memsetAsync.h"
 
 #include "CUDADataFormats/TrackingRecHit/interface/TrackingRecHit2DCUDA.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/launch.h"
 
 #include "RecHitSoATest.h"
+
+/*
+TrackingRecHit2DCUDA and TrackingRecHit2DCPU are NOT the same type (for tracks and vertices are the same type)
+they are templated with the (GPU/CPU)Traits so in principle the whole Analyzer can be (partially) templated as well
+they return the same view type though (so the real analyzer algo in the header file above does not need to be templated)
+*/
+
 
 class RecHitSoATest : public edm::global::EDAnalyzer<> {
 public:
@@ -49,6 +57,10 @@ RecHitSoATest::RecHitSoATest(const edm::ParameterSet& iConfig) : m_onGPU(iConfig
   }
 }
 
+
+/*
+  most of (all?) the code below is boiler-plate. we hope in future to fully wrap it so that a single (templated?) source will work for both gpu and cpu 
+*/
 void RecHitSoATest::analyze(edm::StreamID streamID, edm::Event const& iEvent, const edm::EventSetup& iSetup) const {
   if (m_onGPU) {
     auto const & gh = iEvent.get(tGpuHits);
@@ -60,21 +72,22 @@ void RecHitSoATest::analyze(edm::StreamID streamID, edm::Event const& iEvent, co
     if (0 == nhits) return;
 
     auto ws_d = GPUTraits::make_unique<double[]>(10,ctx.stream());
-    cudaCheck(cudaMemsetAsync(ws_d.get(), 0, 10*sizeof(double), ctx.stream()));
-
+    cudautils::memsetAsync(ws_d, 0, 10, ctx.stream());
+    // in future this bit should go in the .cu as well (otherwise it will launch the cpu version as below!)
+    // in any case we cannot launch on gpu and cpu from the same file (at least with current "technology")
     int threadsPerBlock = 256;
     int blocks = (nhits + threadsPerBlock - 1) / threadsPerBlock;
     cudautils::launch(analyzeHits,{threadsPerBlock,blocks,0,ctx.stream()} , gHits.view(), nhits, ws_d.get());
 
   } else {
-    
+    // MIND most of the (wrapper) types are different w/r/t above!
     auto const & gHits = iEvent.get(tCpuHits);
     auto nhits = gHits.nHits();
 
     if (0 == nhits) return;
 
     auto ws_d = CPUTraits::make_unique<double[]>(10,cudaStreamDefault);
-    ::memset(ws_d.get(), 0, 10*sizeof(double));
+    ::memset(ws_d.get(), 0, 10*sizeof(double));  // in future cudautils::memsetAsync will work as well
     // int threadsPerBlock = 256;
     // int blocks = (nhits + threadsPerBlock - 1) / threadsPerBlock;
     //    cudautils::launch(analyzeHits,{threadsPerBlock,blocks,0,cudaStreamDefault} , gHits.view(), nhits, ws_d.get());
